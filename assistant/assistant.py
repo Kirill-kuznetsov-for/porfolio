6512f1ea712da11840f0a49d02322a95ae5998fe
Kirill-kuznetsov-for <54472543+Kirill-kuznetsov-for@users.noreply.github.com>
2021-11-23 16:19:09 +0300

    Initial commit

---
+++ assistant/assistant.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+import random
+import playsound
+import os
+from gtts import gTTS
+
+
+def listen():
+    voice_recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Скажите что-то >>>")
+        audio = voice_recognizer.listen(source)
+    try:
+        voice_text = voice_recognizer.recognize_google(audio, language="ru")
+        print(f"вы сказали: ...{voice_text}")
+        return voice_text
+    except sr.UnknownValueError:
+        return "ошибка распознания"
+    except sr.RequestError:
+        return "ошибка запроса"
+
+
+def say(text):
+    voice = gTTS(text, lang="ru")
+    unique_file = "audio_" + str(random.randint(0, 10000)) + ".mp3"
+    voice.save(unique_file)
+
+    playsound.playsound(unique_file)
+    os.remove(unique_file)
+
+    print(f"Ассистент: {text}")
+
+
+def handle_command(command):
+    command = command.lower()
+    if command == "Привет":
+        say("Привет-привет")
+    elif command == "пока":
+        stop()
+    else:
+        say("Не понятно, повторите")
+
+
+def stop():
+    say("До скорого")
+    exit()
+
+
+def start():
+    print("запуск ассистента...")
+
+    while True:
+        command = listen()
+        handle_command(command)
+
+
+try:
+    start()
+except KeyboardInterrupt:
+    stop()
